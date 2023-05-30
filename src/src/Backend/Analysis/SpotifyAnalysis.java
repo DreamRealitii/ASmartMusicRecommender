71ee290d57e6617d576a41214e3093ec8ddcaac4
Arnav Srinivasan <arnav.srinivasan@gmail.com>
2023-05-30 14:14:43 -0700

    Song name appears in scroll frame, URL still copied to clipboard

--- src/src/Backend/Analysis/SpotifyAnalysis.java
+++ src/src/Backend/Analysis/SpotifyAnalysis.java
@@ -99,6 +99,8 @@ public class SpotifyAnalysis implements SoundAnalysis {
     return trackId;
   }
 
+  public String getTrackName() { return trackName;}
+
   public String[] getArtistsID(){
     return artistsID;
   }
